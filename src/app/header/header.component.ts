debaca9c4b46aad8764ba653427b056e3adb2322
coryshaw1 <cory.shaw.dev@gmail.com>
2017-02-21 18:52:53 -0500

    update(header): Use collapse from fuel-ui rather than classes from bootstrap

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Collapse } from 'fuel-ui';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [Collapse(350)]
 })
 export class HeaderComponent implements OnInit {
 

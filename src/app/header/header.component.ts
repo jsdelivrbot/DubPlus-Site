cd65d60d219137ef9b37f146cc6d3e0a50f6a3a1
Cory Shaw <cory.shaw.dev@gmail.com>
2017-02-20 21:56:09 -0500

    feat(layout): Setup layout of site, add major components and routes, switch to scss

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Collapse } from 'fuel-ui';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  animations: [Collapse(350)]
+})
+export class HeaderComponent implements OnInit {
+
+  navbarOpen: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
